e2cec9bd1f58ef58c9d19686373a0caf9696e8cf
Louis Perkins <williandwill2@gmail.com>
2021-04-22 11:02:34 -0500

    updated

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+(function () {
   function setQuiz() {
     const output = [];
 
@@ -14,8 +15,10 @@
         );
       }
       output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
+        `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>
+      </div>`
       );
     });
     quizHolder.innerHTML = output.join('');
@@ -40,13 +43,38 @@
     });
     resultsHolder.innerHTML = `${totalCorrect} out of ${myQuestions.length}`;
   }
+
+  function slideShow(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+      preButton.style.display = 'none';
+    } else {
+      preButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    } else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function nextSlide() {
+    slideShow(currentSlide + 1);
+  }
+  function previousSlide() {
+    slideShow(currentSlide - 1);
+  }
   const quizHolder = document.getElementById('quiz');
   const resultsHolder = document.getElementById('results');
   const submitButton = document.getElementById('submit');
 
   const myQuestions = [
     {
-      question: 'Which heading tag offers the biggest output?',
+      question: '<b>(1).</b> Which heading tag offers the biggest output?',
       answers: {
         a: 'h5',
         b: 'h3',
@@ -57,7 +85,7 @@
     },
     {
       question:
-        "What is the index of Nissan?  let cars = ['Ford', 'Nissan', 'Honda', 'GMC']",
+        "(2). What is the index of Nissan?  let cars = ['Ford', 'Nissan', 'Honda', 'GMC']",
       answers: {
         a: 2,
         b: 1,
@@ -67,7 +95,7 @@
       correctAnswer: 'b',
     },
     {
-      question: 'What way[s] can CSS be added to HTML documents?',
+      question: '(3). What way[s] can CSS be added to HTML documents?',
       answers: {
         a: 'External',
         b: 'Internal',
@@ -90,4 +118,13 @@
   ];
   setQuiz();
 
+  const preButton = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  slideShow(currentSlide);
   submitButton.addEventListener('click', visibleResults);
+  preButton.addEventListener('click', previousSlide);
+  nextButton.addEventListener('click', nextSlide);
+})();
